83716b3858df7d1315f44ca201ae349ae9796ea3
Olivier Filangi <olivier.filangi@gmail.com>
2021-01-21 21:24:55 +0100

    fix

--- update_cdn_libjs.sh
+++ update_cdn_libjs.sh
@@ -6,4 +6,8 @@ cp js/target/scala-2.13/discovery-opt.js dist/discovery.js
 sed -i "s#$(pwd)#com/github/p2m2#g" dist/discovery.js
 
 ./node_modules/.bin/browserify -r ./dist/discovery.js -s discovery > dist/discovery-web.js
-git commit dist/discovery.js dist/discovery-web.js -m"update cdn js lib." &>/dev/null
+
+if [ -z "$var" ];then
+  echo " -- commit -- "
+  git commit dist/discovery.js dist/discovery-web.js -m"update cdn js lib." &>/dev/null
+fi
