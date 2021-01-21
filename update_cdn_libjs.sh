86392fa2e524a35de77bf5bef00a5c7a9b3683af
Olivier Filangi <olivier.filangi@gmail.com>
2021-01-21 21:36:16 +0100

    fix

--- update_cdn_libjs.sh
+++ update_cdn_libjs.sh
@@ -7,6 +7,8 @@ sed -i "s#$(pwd)#com/github/p2m2#g" dist/discovery.js
 
 ./node_modules/.bin/browserify -r ./dist/discovery.js -s discovery > dist/discovery-web.js
 
+# var CI defined on circleci at true
+
 if [ -z "$CI" ];then
   echo " -- commit -- "
   git commit dist/discovery.js dist/discovery-web.js -m"update cdn js lib." &>/dev/null
