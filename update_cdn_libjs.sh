07ea1f90de40a0fc2fe00979c32602a6f8914021
Olivier Filangi <olivier.filangi@gmail.com>
2021-01-21 23:07:47 +0100

    fix ci

--- update_cdn_libjs.sh
+++ update_cdn_libjs.sh
@@ -9,7 +9,7 @@ sed -i "s#$(pwd)#com/github/p2m2#g" dist/discovery.js
 
 # generate md5sum to check js libraries
 
-cat $(find . -name SWD*.scala | sort | grep -v SWDiscoveryVersionAtBuildTime.scala) | md5sum > dist/checksum
+cat $(find . -name *.scala | sort | grep -v SWDiscoveryVersionAtBuildTime.scala) | md5sum > dist/checksum
 
 # var CI defined on circleci at true
 
