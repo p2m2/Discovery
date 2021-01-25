f7b0f2e2a46a3a87c9f8bc9b319ff1c0802957b4
Olivier Filangi <olivier.filangi@gmail.com>
2021-01-25 12:32:58 +0100

    fix request test

--- jvm/src/test/scala/inrae/semantic_web/driver/SHTTPDriverTest.scala
+++ jvm/src/test/scala/inrae/semantic_web/driver/SHTTPDriverTest.scala
@@ -10,7 +10,7 @@ object SHTTPDriverTest extends TestSuite {
 
   DataTestFactory.delete_virtuoso1(this.getClass.getSimpleName)
 
-  DataTestFactory.insert_virtuoso1(
+  val insert_data = DataTestFactory.insert_virtuoso1(
     """
       <aaSHTTPDriverTestDriverTest> <bb> <cc> .
       """.stripMargin, this.getClass.getSimpleName)
@@ -23,59 +23,72 @@ object SHTTPDriverTest extends TestSuite {
   def tests = Tests {
 
     test("get") {
+      insert_data.map(_ => {
       SHTTPDriver().get(query, ConfigurationHttpRequest(url = DataTestFactory.url_endpoint))
         .map(qr => {
           assert(qr.json("results")("bindings").arr(0)("b")("value").value == "bb")
           assert(qr.json("results")("bindings").arr(0)("c")("value").value == "cc")
-        }
-        )
+        })
+      }).flatten
     }
 
     test("get bad request") {
+      insert_data.map(_ => {
       SHTTPDriver().get("bad request", ConfigurationHttpRequest(url = DataTestFactory.url_endpoint))
         .map(_ => assert(false))
         .recover(v => assert(true))
+      }).flatten
     }
 
     test("get malformed endpoint") {
-      SHTTPDriver().get(query, ConfigurationHttpRequest(url = "bidon"))
-        .map(_ => assert(false))
-        .recover(_ => assert(true))
+      insert_data.map(_ => {
+        SHTTPDriver().get(query, ConfigurationHttpRequest(url = "bidon"))
+          .map(_ => assert(false))
+          .recover(_ => assert(true))
+      }).flatten
     }
 
     test("get endpoint does not exist") {
+      insert_data.map(_ => {
       SHTTPDriver().get(query, ConfigurationHttpRequest(url = "http://bidon.com"))
         .map(_ => assert(false))
         .recover(_ => assert(true))
+      }).flatten
     }
 
     test("post") {
+      insert_data.map(_ => {
       SHTTPDriver().post(query, ConfigurationHttpRequest(url = DataTestFactory.url_endpoint))
         .map(qr => {
           assert(qr.json("results")("bindings").arr.length > 0)
           assert(qr.json("results")("bindings").arr(0)("b")("value").value == "bb")
           assert(qr.json("results")("bindings").arr(0)("c")("value").value == "cc")
         })
+      }).flatten
     }
 
     test("post bad request") {
-      //NOSONAR
+      insert_data.map(_ => {
       SHTTPDriver().post("bad request", ConfigurationHttpRequest(url = DataTestFactory.url_endpoint))
         .map(_ => assert(false))
         .recover(_ => assert(true))
+      }).flatten
     }
 
     test("post malformed endpoint") {
-      //NOSONAR
+      insert_data.map(_ => {
       SHTTPDriver().post(query, ConfigurationHttpRequest(url = "bidon"))
         .map(_ => assert(false))
         .recover(_ => assert(true))
+      }).flatten
     }
 
     test("post endpoint does not exist") {
+      insert_data.map(_ => {
       SHTTPDriver().post(query, ConfigurationHttpRequest(url = "http://bidon.com"))
         .map(_ => assert(false))
         .recover(_ => assert(true))
+      }).flatten
     }
   }
 }
