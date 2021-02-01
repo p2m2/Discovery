a386fcc4ed0d2969bcf1ca843437e65f99997109
Olivier Filangi <olivier.filangi@gmail.com>
2021-02-01 10:18:52 +0100

    test circleci

--- update_cdn_libjs.sh
+++ update_cdn_libjs.sh
@@ -5,9 +5,14 @@ rm dist/discovery*.js
 # generate build version
 rm ./shared/src/main/scala/inrae/semantic_web/SWDiscoveryVersionAtBuildTime.scala
 
+# browser lib debug information
 sbt discoveryJS/fastOptJS::webpack
+# node lib
+sbt discoveryJS/fullOptJS
+# browser lib
 sbt discoveryJS/fullOptJS::webpack
 
+# browser lib js
 cp ./js/target/scala-2.13/scalajs-bundler/main/discovery-fastopt-bundle.js  ./dist/discovery-web-dev.js
 cp ./js/target/scala-2.13/scalajs-bundler/main/discovery-fastopt-bundle.js.map ./dist/discovery-web-dev.js.map
 cp ./js/target/scala-2.13/scalajs-bundler/main/discovery-opt-bundle.js ./dist/discovery-web.js
