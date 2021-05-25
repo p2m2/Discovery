d0f68aa23048e537e528d8c0ccc07de61933a478
Olivier Filangi <olivier.filangi@gmail.com>
2021-05-25 13:39:00 +0200

    update post jvm side

--- jvm/src/test/scala/inrae/data/PostRequest.scala
+++ jvm/src/test/scala/inrae/data/PostRequest.scala
@@ -1,70 +1,26 @@
 package inrae.data
 
-import java.io.{BufferedReader, BufferedWriter, InputStreamReader, OutputStreamWriter}
-import java.net.{HttpURLConnection, URL}
+
+import org.apache.http.NameValuePair
+import org.apache.http.client.entity.UrlEncodedFormEntity
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.impl.client.HttpClientBuilder
+import org.apache.http.message.BasicNameValuePair
+
+import java.util
 import scala.concurrent.Future
 
 
-abstract class SimpleHttpRequest {
-  type Param = Map[String, String]
-
-  def post(url:String, data:Param):String
-  def post(url:String, data:Param, headers:Param):String
-}
-
-
-case object PostRequest extends SimpleHttpRequest {
+case object PostRequest {
   implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
-  def post(url:String, data:Param):String = this.post(url, data, null)
-  def post(url:String, data:Param, headers:Param):String = {
-    try {
-      val http = this.getHttpURLConnection(url, "POST", headers)(h => {
-        h.setDoOutput(true)
-      })
 
-      val bw = new BufferedWriter(new OutputStreamWriter(http.getOutputStream))
-      try {
-        bw.write(data.toString)
-        bw.flush
-      } finally {
-        bw.close
-      }
+  def put(stringQuery: String, url_endpoint: String) : Future[Any] = {
+    val post = new HttpPost(url_endpoint)
+    val nameValuePairs = new util.ArrayList[NameValuePair]()
+    nameValuePairs.add(new BasicNameValuePair("query", stringQuery))
+    post.setEntity(new UrlEncodedFormEntity(nameValuePairs))
+    val client = HttpClientBuilder.create.build()
 
-      this.getContentsBody(http)
-    } catch {
-      case _ => null
-    }
-  }
-
-
-  protected def getHttpURLConnection(url:String, method:String, headers:Param)
-                                    (implicit interrupt:HttpURLConnection => Unit):HttpURLConnection = {
-    val http = (new URL(url)).openConnection.asInstanceOf[HttpURLConnection]
-    http.setRequestMethod(method)
-    http.setInstanceFollowRedirects(false)
-    if(headers != null){
-      headers.foreach(h => http.setRequestProperty(h._1, h._2))
-    }
-    interrupt(http)
-    http.connect
-    http
-  }
-
-  protected def getContentsBody(http:HttpURLConnection):String = {
-    val br = new BufferedReader(new InputStreamReader(http.getInputStream))
-    val sb = new StringBuilder(1024)
-    var line = ""
-    try {
-      while({line = br.readLine; line != null}){
-        sb.append(line + "\n")
-      }
-    } finally {
-      br.close
-    }
-    sb.toString
-  }
-
-  def put(stringQuery: String, url_endpoint: String) : Future[String] = {
-    Future { post(url_endpoint, Map("query" -> stringQuery)) }
+    Future { client.execute(post) }
   }
 }
